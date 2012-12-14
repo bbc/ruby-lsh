74f473cab28d7073a935d8c283000a28cebeafbf
Yves Raimond <yves.raimond@bbc.co.uk>
2012-12-14 16:42:59 +0000

    Testing norm

--- test/test_math_util.rb
+++ test/test_math_util.rb
@@ -20,4 +20,10 @@ class MathUtilTest < Test::Unit::TestCase
     assert_equal Float, math.dot(math.random_gaussian_vector(10), math.random_gaussian_vector(10)).class
   end
 
+  def test_norm
+    math = LSH::MathUtil.new
+    v = math.random_gaussian_vector(10)
+    assert_equal 1.0, math.norm(v / math.norm(v)).round(4)
+  end
+
 end
