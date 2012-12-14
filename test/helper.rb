b1c9ba7d8879fbcf342132cea61b521842403737
Yves Raimond <yves.raimond@bbc.co.uk>
2012-12-14 15:18:29 +0000

    Some (very light) tests for the jblas/gsl wrapper

---
+++ test/helper.rb
@@ -0,0 +1,4 @@
+require 'test/unit'
+require 'test/unit/assertions'
+
+require File.join(File.dirname(__FILE__), '..', 'lib', 'lsh')
---
+++ test/test_math_util.rb
@@ -0,0 +1,23 @@
+require 'helper'
+
+class MathUtilTest < Test::Unit::TestCase
+
+  def test_random_uniform
+    math = LSH::MathUtil.new
+    20.times do |i|
+      assert (math.random_uniform >= 0)
+      assert (math.random_uniform <= 1)
+    end
+  end
+
+  def test_random_gaussian_vector
+    math = LSH::MathUtil.new
+    assert_equal 10, math.random_gaussian_vector(10).size
+  end
+
+  def test_dot
+    math = LSH::MathUtil.new
+    assert_equal Float, math.dot(math.random_gaussian_vector(10), math.random_gaussian_vector(10)).class
+  end
+
+end
